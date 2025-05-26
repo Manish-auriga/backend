33e2e3c7d1c68c000cc9e11d35a0cfabc1e2e2ec
Manish-auriga <manish.sharma@aurigait.com>
2025-05-26 14:28:49 +0530

    automate.sh file updation

--- automate.sh
+++ automate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 NAME=manish22072004
+cd /home/manish_sharma/app
 echo $docker_password | sudo docker login --username manish22072004 --password-stdin
 
 # sudo docker build -t $NAME/frontend:$TIMESTAMP ./frontend
 # ../
 sudo docker build -t $NAME/backendprivate:$TIMESTAMP .
-../
+
 
 # sudo docker push $NAME/frontend:$TIMESTAMP 
 sudo docker push $NAME/backendprivate:$TIMESTAMP 
@@ -19,4 +20,4 @@ sudo docker rmi $NAME/backendprivate:$TIMESTAMP
 sed -i "s|image: $NAME/backendprivate:.*|image: $NAME/backendprivate:$TIMESTAMP|" docker-compose.yml
 
 sudo docker compose down 
-sudo docker compose up 
+sudo docker compose up -d
